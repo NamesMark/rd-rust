8535093ded8aef2291c6867b95e16ab324a7bc1c
NamesMark <markfirn@gmail.com>
2023-10-03 19:20:36 +0300

    Implements improved Hello world

--- hw-01/src/main.rs
+++ hw-01/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-    println!("Hello, world!");
+    let text_1: String = "Hello ".to_string();
+    let text_2: &str = "World";
+    let text_3: char = '!';
+
+    print_any(&text_1);
+    print_any(text_2);
+    print_any(&text_3);
+}
+
+fn print_any<T: std::fmt::Display + ?Sized>(text: &T) {
+    print!("{}", text);
 }
